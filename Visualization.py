8ec2f4f68c39c223e8343258cd21bbefbb625146
ollevirding <olle.virding@gmail.com>
2023-02-07 11:30:44 +0100

    Visualization

---
+++ Visualization.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sklearn.preprocessing as skl_pre
+import sklearn.linear_model as skl_lm
+import sklearn.discriminant_analysis as skl_da
+import sklearn.neighbors as skl_nb
+import sklearn.model_selection as skl_ms
+
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
